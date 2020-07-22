cc1d35f233b1655c0f0b772b0843f350c4394f8f
Dima Chumak <chumakd@gmail.com>
2020-07-22 23:45:28 +0300

    fix backup and autosave

--- init.el
+++ init.el
@@ -129,8 +129,8 @@
 (setq use-dialog-box nil)
 
 ;; backups location
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup")))
-(setq auto-save-file-name-transforms  '(("." "~/.emacs.d/backup" t)))
+(setq backup-directory-alist '((".*" . "~/.emacs.d/backup/")))
+(setq auto-save-file-name-transforms  '((".*" "~/.emacs.d/backup/\\2" t)))
 
 ;; weeks should begin on Monday
 (setq calendar-week-start-day 1)
