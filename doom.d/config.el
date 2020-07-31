4d7987910e95ab1a0a4174651e9a33cb59392dd7
Dima Chumak <chumakd@gmail.com>
2020-07-31 20:17:46 +0300

    doom: add lsp-treemacs mode

--- doom.d/config.el
+++ doom.d/config.el
@@ -58,3 +58,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(use-package! lsp-treemacs
+    :defer t
+    :config
+    (lsp-treemacs-sync-mode 1))
