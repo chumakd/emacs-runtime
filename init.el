1dc95b3c70aeeb1f269dfd374798b98ee1b0cd7a
Dima Chumak <chumakd@gmail.com>
2023-11-19 12:53:40 +0100

    update melpa config

--- init.el
+++ init.el
@@ -12,7 +12,8 @@
 
 ;; repositories
 (setq         package-archives  '(("gnu"   . "https://elpa.gnu.org/packages/")))
-(add-to-list 'package-archives  '( "melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 ;(add-to-list 'package-archives  '( "marmalade" . "https://marmalade-repo.org/packages/"))
 
 ;; activate all the packages
